9ce19bf69782fd0c8866763d3b11a4dda360a7d3
PosaLusa24 <posalusa24@gmail.com>
2020-12-10 16:23:43 +0630

    Fix rpi camera support

--- fridgeai/__main__.py
+++ fridgeai/__main__.py
@@ -1,12 +1,12 @@
 """Entry point of program."""
 import sys
-from PyQt5 import QtWidgets
-from fridgeai import shell
-from fridgeai.gui.mainwindow import Ui_MainWindow
 
 if len(sys.argv) == 2 and sys.argv[1] == "--shell":
+    from fridgeai import shell
     shell.init()
 else:
+    from PyQt5 import QtWidgets
+    from fridgeai.gui.mainwindow import Ui_MainWindow
     app = QtWidgets.QApplication(sys.argv)
     fridgeai_window = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
--- fridgeai/camera.py
+++ fridgeai/camera.py
@@ -1,5 +1,6 @@
 import platform
 import cv2
+import time
 if platform.machine() == "armv6l":
     import picamera
 
@@ -15,34 +16,25 @@ def get_frames(shape, count, interval):
 def _get_frames_picamera(shape, count, interval):
     frames = []
 
-    camera = picamera.PiCamera()
-    camera.resolution = (640, 480)
-    camera.framerate = 30
-    raw_capture = picamera.PiRGBArray(camera, size=(640, 480))
+    with picamera.PiCamera() as camera:
+        with picamera.array.PiRGBArray(camera, size=(32, 32)) as output:
+            camera.resolution = (480, 640)
+            camera.start_preview()
+            for i, frame in enumerate(camera.capture_continuous(
+                output, resize=(32, 32), format="bgr", use_video_port=True
+            )):
+                # output=output.reshape(32,32,3)
+                # frame = cv2.resize(
+                #     frame.array, (32,32), interpolation=cv2.INTER_AREA
+                # )
+                frames.append(frame)
+                print(frame.shape)
+                if i == 8:
+                    break
+                output.truncate(0)
+                time.sleep(0.4)
+            camera.stop_preview()
 
-    for i, frame in enumerate(camera.capture_continuous(
-            raw_capture,
-            format="bgr",
-            use_video_port=True)):
-        if (i > count * interval):
-            break
-        frame = frame.array
-        preview_frame = cv2.flip(frame, 1)
-        if i % interval == 0:
-            frames.append(cv2.resize(frame, shape))
-            # turn the preview frame white to simulate a camera flash
-            preview_frame = cv2.rectangle(
-                preview_frame,
-                (0, 0),
-                (preview_frame.shape[1], preview_frame.shape[0]),
-                (255, 255, 255),
-                -1
-            )
-        cv2.imshow("FridgeAI Camera", preview_frame)
-        cv2.waitKey(1)
-        raw_capture.truncate(0)
-
-    cv2.destroyAllWindows()
     return frames
 
 
